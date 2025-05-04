5cf588881a0b12fd291ee4181b6dd26730c073c0
Aaron C <aaronc@Aarons-MacBook-Air.local>
2025-05-04 06:49:38 -0400

    UPDATE: added react forms for rendering efficiency and updated components to match MUI styling

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -1,23 +1,55 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, TextField, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
+type LoginFormInputs = {
+    email: string;
+    password: string;
+}
 
 
 const LoginForm: React.FC = () => {
     // create checkboxes for remember me and forgot password
     // create a function to handle form submission
     // create variables to handle Fetch requests and API calls
+    const { register, handleSubmit, formState: { errors } } = useForm<LoginFormInputs>();
+    const onSubmit = (values: any) => {
+        console.log(values);}
     return (
         <Box>
-            <h1>Login</h1>
-            <form>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" required />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" required />
-                </div>
+            <Typography>Login</Typography>
+            <form onSubmit = {handleSubmit(onSubmit)}>
+                <Box>
+                    <TextField 
+                    fullWidth
+                    label="email"
+                    type="email"
+                    {...register('email',{
+                        required: 'Email is required',
+                        pattern: {
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
+                            message: 'Invalid email address'
+                        }
+                    })}
+                    error={!!errors.email}
+                    helperText = {errors.email?.message}
+                    />
+                </Box>
+                <Box>
+                    <TextField fullWidth
+                    label="password"
+                    type="password"
+                    {...register('password', {
+                        required: 'Password is required',
+                        minLength: {
+                            value: 6,
+                            message: 'Password must be at least 6 characters long'
+                        }
+                    })}
+                    error={!!errors.email}
+                    helperText = {errors.email?.message}
+                    />
+                </Box>
                 <button type="submit">Login</button>
             </form>
         </Box>
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import '../Styles/NavStyling.scss';
+import navStyles from '../Styles/NavStyling.module.scss';
+import { Link } from 'react-router-dom';
+import { Box, AppBar,Toolbar, Button } from '@mui/material';
 
 
 
 const Navbar: React.FC = () => {
     return (
-        <nav className='navbar'>
-            <ul>
-                <li className='nav-item'><a href="/">Home</a></li>
-                <li className='nav-item'><a href="/about">About</a></li>
-                <li className='nav-item'><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
+        <AppBar className={navStyles.navbar} position='static'>
+            <Toolbar className={navStyles.navbar_toolbar}>
+                <Box className={navStyles.nav_box}>
+                    <Button className={navStyles.nav_item} component={Link} to="/">Home</Button>
+                    <Button className={navStyles.nav_item}component={Link} to="/about">About</Button>
+                    <Button className={navStyles.nav_item}component={Link} to="/contact">Contact Us</Button>
+                    <Box>
+                        <Button className={navStyles.nav_item}component={Link} to="/login">Login</Button>
+                        <Button className={navStyles.nav_item}component={Link} to="/login">Sign Up</Button>
+                    </Box>
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 };
 
